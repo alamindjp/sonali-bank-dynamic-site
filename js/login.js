0116c21c13d140cbe61341cf309e0e0f88a57601
alamindjp <alaminhossaindjp@gmail.com>
2022-02-12 00:46:46 +0600

    update site

--- js/login.js
+++ js/login.js
@@ -2,13 +2,12 @@ document.getElementById('login-btn').addEventListener('click', function () {
     //    get user email 
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    console.log(userEmail)
     // get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
     // error handeler 
-    if (userEmail == 'sonali.com' && userPassword == 'a') {
+    if (userEmail == 'alaminhossaindjp@gmail.com' && userPassword == 'Alamin.hossain') {
         window.location.href = 'banking.html';
     }
     else {
